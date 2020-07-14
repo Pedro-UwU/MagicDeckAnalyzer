d9f14f9fb79f0c726e0872a9726f5d131c64bfb0
PedroLopezITBA <pelopez@itba.edu.ar>
2020-07-13 23:47:29 -0300

    Changes in MainFrame

--- src/Front/MainFrame.java
+++ src/Front/MainFrame.java
@@ -1,6 +1,9 @@
 package Front;
 
+import Back.fileManager.DeckReader;
 import javafx.geometry.Insets;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -15,20 +18,20 @@ public class MainFrame extends BorderPane {
         DropDownDecks deckList = new DropDownDecks(decks);
         Desk desk = new Desk(10, width);
 
-        Button deckPrinter = new Button("Print Deck");
-        deckPrinter.setPrefWidth(3*width/8);
-        Label deckVisualizer = new Label("No deck Selected");
-        ScrollPane deckScroll = new ScrollPane(deckVisualizer);
-        deckScroll.setFitToWidth(true);
-        deckVisualizer.setPrefWidth(width);
-        deckPrinter.setOnAction(event -> {
-                try {
-                    deckVisualizer.setText(deckList.getSelectedDeck().toStringWithRepeated());
-                } catch (Exception e) {
-                    deckVisualizer.setText("Please Select Deck");
-                }
-            });
-        desk.getChildren().addAll(deckPrinter, deckScroll);
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+        MatchLogChart<String,Number> chart = new MatchLogChart<>(xAxis,yAxis);
+        chart.setPrefWidth(width/2);
+
+        Button readDeckLogButton = new Button("Read Deck Log File");
+        readDeckLogButton.setOnAction(event -> {
+            if (deckList.validSelection()) DeckReader.ReadDeckLog(deckList.getValue());
+        });
+
+
+        desk.getChildren().addAll(chart, readDeckLogButton);
+
+
 
         this.setPrefWidth(width);
         this.setPrefHeight(height);
